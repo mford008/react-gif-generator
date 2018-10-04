8e6019c5a3d11fdaa3db4bce6c0fbf0dafb12e20
Maddy Ford <mrhf008@gmail.com>
2018-10-04 11:08:31 -0700

    trending gif data rendering on loading page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Search from './components/Search/Search.js';
+import TileContainer from './components/TileContainer/TileContainer.js';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Shift Gif Generator</h1>
         <Search />
+        <TileContainer />
       </div>
     );
   }
