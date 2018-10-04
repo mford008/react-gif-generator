87d657a1484103a45a44989c435a64089a4c5c54
Maddy Ford <mrhf008@gmail.com>
2018-10-04 10:48:54 -0700

    search function working

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,29 +2,17 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Button extends Component {
-  onClickButton = event => {
-    event.preventDefault();
-    console.log('hello')
-    axios.get('https://api.giphy.com/v1/gifs/trending?api_key=***REMOVED***')
-    .then(res => {
-      const gifs = res.data;
-      console.log(gifs.data[0].title);
-      console.log(gifs.data[0].rating);
-      console.log(gifs.data[0].username);
-      console.log('success');
-    });
   //   if (this.state.isPurple) {
   //     this.setState({isPurple:false})
   //   }
   //   else (this.state.isPurple) {
   //     this.setState({isPurple:true})
   //   }
-  }
 
   render() {
     return(
       <div>
-        <button onClick={this.onClickButton}>{this.props.children}</button>
+        <button onClick={this.props.onClick}>{this.props.children}</button>
       </div>
     )
   }
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -16,3 +16,18 @@ class Tile extends Component {
 }
 
 export default Tile;
+
+// onClickButton = event => {
+//   event.preventDefault();
+//   console.log('hello')
+//   axios.get('https://api.giphy.com/v1/gifs/trending?api_key=***REMOVED***')
+//   .then(res => {
+//     const gifs = res.data;
+//     console.log(gifs.data[0].title);
+//     console.log(gifs.data[0].rating);
+//     console.log(gifs.data[0].username);
+//     console.log('success');
+//   });
+
+// <button onClick={this.onClickButton}>{this.props.children}</button>
+//
