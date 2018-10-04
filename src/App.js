5046986bd26afc4ba9ae22036833aa80a9b50667
Maddy Ford <mrhf008@gmail.com>
2018-10-03 19:04:29 -0700

    search field added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    term: ''
+  }
+  handleInputChange = (ev) => {
+    let value = ev.target.value;
+    this.setState({
+      name: value,
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>Shift Gif Generator</h1>
+        <input placeholder="Enter your Search Term here"
+               onChange={this.handleInputChange} />
         <Button>Hello</Button>
         <Tile />
       </div>
