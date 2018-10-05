dbe8a8addb37b412ff5b8d0b8d80183324f783ab
Maddy Ford <mrhf008@gmail.com>
2018-10-05 06:49:39 -0700

    moving state of search to app

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,21 +3,21 @@ import Button from '../Button/Button.js';
 import axios from 'axios';
 
 class Search extends React.Component {
-  state = {
-    term: '',
-    input: '',
-  }
-  handleInputChange = (ev) => {
-    let value = ev.target.value;
-    this.setState({term: value});
-    console.log(this.state.term);
-  }
+  // state = {
+  //   term: '',
+  //   input: '',
+  // }
+  // handleInputChange = (ev) => {
+  //   let value = ev.target.value;
+  //   this.setState({term: value});
+  //   console.log(this.state.term);
+  // }
 
   render() {
     return (
       <div className="Search">
         <input placeholder="Enter your Search Term here"
-               onChange={this.handleInputChange} />
+               onChange={this.props.handleInputChange} />
         <Button onClick={this.props.onClickButton}>Search</Button>
       </div>
     );
