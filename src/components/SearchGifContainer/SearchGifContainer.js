7a093779b4edf29acfd5b5ecfc5f0a30d445dd36
Maddy Ford <mrhf008@gmail.com>
2018-10-05 06:54:13 -0700

    correctly altering state of searched-gifs and rendering gifs based on search term

--- src/components/SearchGifContainer/SearchGifContainer.js
+++ src/components/SearchGifContainer/SearchGifContainer.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Gif from '../Gif/Gif.js';
 
 const SearchGifContainer = (props) => {
-    const searchedGifs = props.searchedGifs.map((image) => {
+    const searched_gifs = props.searched_gifs.map((image) => {
         return <Gif key={image.id}
                         gif={image} />
     });
 
     return (
-        <div className="gif-container">{searchedGifs}</div>
+        <div className="gif-container">{searched_gifs}</div>
     );
 };
 
